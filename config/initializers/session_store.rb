c58173f2f49000a4cf2f458f227a69659cc86a76
Francis Hinson <francis@orchive.com>
2012-02-12 20:13:01 -0500

    fixed quiz! i'm a bosssss

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Tussify::Application.config.session_store :cookie_store, key: '_Tussify_session'
-
+Tussify::Application.config.session_store :active_record_store
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
